0db2b4f97ee1c64caadb00b233c95388a4368ad1
BenedetArthur <arthurB77@free.fr>
2019-05-21 11:25:14 +0200

    création dossiers et pages

---
+++ PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageAccueil.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PPE4GSB.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageAccueil : ContentPage
+    {
+        public PageAccueil()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnListeMedicament_Clicked(object sender, EventArgs e)
+        {
+            Pages.PageMedicament page = new Pages.PageMedicament();
+            await Navigation.PushModalAsync(page);
+        }
+
+        private async void btnListeIndividu_Clicked(object sender, EventArgs e)
+        {
+            Pages.PageListIndividu page = new Pages.PageListIndividu();
+            await Navigation.PushModalAsync(page);
+        }
+
+        private async void btnPrescription_Clicked(object sender, EventArgs e)
+        {
+            Pages.PagePrescription page = new Pages.PagePrescription();
+            await Navigation.PushModalAsync(page);
+        }
+    }
+}
