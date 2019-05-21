fcfd8af940d0eb502526dd8452ea7012b077dfde
BenedetArthur <arthurB77@free.fr>
2019-05-21 15:50:28 +0200

    Création de fichiers pour services web

--- PPE4GSB/PPE4GSB/PPE4GSB/App.xaml.cs
+++ PPE4GSB/PPE4GSB/PPE4GSB/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,6 +9,12 @@ namespace PPE4GSB
 {
     public partial class App : Application
     {
+        public static string LocalHost { get; set; }
+        public static List<Medicaments> LesMedicaments { get; set; }
+        public static List<Individu_type> LesTypesIndividus { get; set; }
+        public static GestPrescription UnePrescription { get; set; }
+
+
         public App()
         {
             InitializeComponent();
---
+++ PPE4GSB/PPE4GSB/PPE4GSB/GestWebServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPE4GSB
+{
+    public class GestWebServices
+    {
+    }
+}
