8fbd152508f3f74a331974695916ba24535b2bf3
BenedetArthur <arthurB77@free.fr>
2019-05-21 18:45:20 +0200

    Affichage liste medic et correction classe médic

--- PPE4GSB/PPE4GSB/PPE4GSB/Classes/Medicaments.cs
+++ PPE4GSB/PPE4GSB/PPE4GSB/Classes/Medicaments.cs
@@ -6,11 +6,11 @@ namespace PPE4GSB
 {
     public class Medicaments
     {
-        public string med_depotlocal { get; set; }
-        public string med_nomCommercial { get; set; }
-        public string med_contrIndic { get; set; }
-        public string med_composition { get; set; }
-        public string med_effets { get; set; }
-        public double med_prixechantillon { get; set; }
+        public string Med_depotlocal { get; set; }
+        public string Med_nomCommercial { get; set; }
+        public string Med_contrIndic { get; set; }
+        public string Med_composition { get; set; }
+        public string Med_effets { get; set; }
+        public double Med_prixechantillon { get; set; }
     }
 }
