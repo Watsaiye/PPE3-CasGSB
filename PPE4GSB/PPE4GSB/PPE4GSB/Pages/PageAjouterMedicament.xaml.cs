8282881c2de9845639d350c7498253cd53b51905
BenedetArthur <arthurB77@free.fr>
2019-05-24 17:27:25 +0200

    PageAjouterMedicament

---
+++ PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageAjouterMedicament.xaml.cs
@@ -0,0 +1,34 @@
+﻿using PPE4GSB.Classes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PPE4GSB.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageAjouterMedicament : ContentPage
+	{
+		public PageAjouterMedicament ()
+		{
+			InitializeComponent ();
+		}
+
+        private void BtnAjouterLeComposant_Clicked(object sender, EventArgs e)
+        {
+            if (txtDepotLegal.Text == null || txtNomCommercial.Text == null || txtComposition.Text == null || txtEffets.Text == null || txtContreIndic.Text == null || txtPrix.Text == null || txtCodeFamille.Text == null)
+            {
+                DisplayAlert("Saisir toutes les informations", "Veuillez saisir toutes les informations nécéssaire", "Valider");
+            }
+            else
+            {
+                App.GestWeb.InsertMedicamentAsync(txtDepotLegal.Text, txtNomCommercial.Text);
+                DisplayAlert("L'insertion s'est bien passé", "Le composant à été ajouté", "Ok");
+            }
+        }
+    }
+}
--- PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageMedicament.xaml.cs
+++ PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageMedicament.xaml.cs
@@ -38,7 +38,7 @@ namespace PPE4GSB.Pages
 
         private async void BtnAjouterMedicament_Clicked(object sender, EventArgs e)
         {
-            Pages.PageAjouterMedicament page = new Pages.PageAjouterMedicament((lvMedicaments.ItemsSource as List<Medicaments>).Count);
+            Pages.PageAjouterMedicament page = new Pages.PageAjouterMedicament(lvMedicaments.ItemsSource as List<Medicaments>);
             await Navigation.PushModalAsync(page);
         }
 
