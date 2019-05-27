d9484c0f8bdbd014a13de71d49f3fad9b79a03cf
BenedetArthur <arthurB77@free.fr>
2019-05-27 14:51:24 +0200

    affichage individu type et ajout modif individu type

--- PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageListIndividu.xaml.cs
+++ PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageListIndividu.xaml.cs
@@ -18,6 +18,22 @@ namespace PPE4GSB.Pages
         public PageListIndividu()
         {
             InitializeComponent();
+            AfficherIndividu();
+        }
+
+        public async void AfficherIndividu()
+        {
+            lvIndividus.ItemsSource = await App.GestWeb.GetIndividu_TypesAsync();
+        }
+
+        private async void LvIndividu_Selected(object sender, EventArgs e)
+        {
+            if (lvIndividus.SelectedItem != null)
+            {
+                Individu_type lIndividuSelectionner = (lvIndividus.SelectedItem as Individu_type);
+                Pages.PageModifierIndividu page = new Pages.PageModifierIndividu(lIndividuSelectionner);
+                await Navigation.PushModalAsync(page);
+            }
         }
 
         private async void BtnAccueil_Clicked(object sender, EventArgs e)
---
+++ PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageModifierIndividu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using PPE4GSB.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PPE4GSB.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageModifierIndividu : ContentPage
+	{
+		public PageModifierIndividu (Individu_type lIndividuSelectionner)
+		{
+			InitializeComponent ();
+            AfficherInfoIndividu(lIndividuSelectionner);
+		}
+
+        public void AfficherInfoIndividu(Individu_type lIndividuSelectionner)
+        {
+            txtCode_Type.Text = lIndividuSelectionner.Code_Type.ToString();
+            txtLibelle_Type.Text = lIndividuSelectionner.Libelle_Type;
+        }
+	}
+}
