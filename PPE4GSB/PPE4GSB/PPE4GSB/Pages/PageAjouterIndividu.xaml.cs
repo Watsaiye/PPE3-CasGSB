0e155995df2793034c7467d1368b96f909707376
BenedetArthur <arthurB77@free.fr>
2019-05-27 15:47:34 +0200

    tentative de fix #01

--- PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageAjouterIndividu.xaml.cs
+++ PPE4GSB/PPE4GSB/PPE4GSB/Pages/PageAjouterIndividu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PPE4GSB.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,14 @@ namespace PPE4GSB.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PageAjouterIndividu : ContentPage
 	{
-		public PageAjouterIndividu ()
+		public PageAjouterIndividu()
 		{
-			InitializeComponent ();
+			InitializeComponent();
 		}
 
         private void BtnAjouterIndividu_Clicked(object sender, EventArgs e)
         {
-            if (txtLibelletype.Text == null)
+            if(txtLibelletype.Text == null)
             {
 
             }
